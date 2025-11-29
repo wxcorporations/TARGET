256efdd3302281d58312ccf0cbfa941353565998
fernando j <fer.j.doc@gmail.com>
2025-11-29 12:54:33 -0300

    feat: header com menu
    - atualizando header para versao deskto e mobile.
    - add menu flutuante na versao mobile.
    - navegacao funcional

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbarModule,
     MatIconModule,
+    MatMenuModule
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
