6038058858b1dc63146e38ee1e6fbe22d3fbadc2
fernando j <fer.j.doc@gmail.com>
2025-11-30 15:34:26 -0300

    feat: remocao de pag home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,15 @@
 import { Routes } from '@angular/router';
 
-// import { Home } from './pages/home/home';
-import { Home } from './presentation/pages/home/home';
 import { Stock } from './presentation/pages/stock/stock';
 import { Commissions } from './presentation/pages/commissions/commissions';
 
-
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home
-    },
     {
         path: 'commissions',
         component: Commissions
     },
     {
-        path: 'stock',
+        path: '',
         component: Stock
     }
 ];
