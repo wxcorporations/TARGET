94e85e5272d995569c13f3ba930739bf72840b32
fernando j <fer.j.doc@gmail.com>
2025-11-30 09:21:25 -0300

    feat: remocao pagina sale
    - funcinalidade descontinuada [tempo]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 
 // import { Home } from './pages/home/home';
 import { Home } from './presentation/pages/home/home';
-import { Sales } from './presentation/pages/sales/sales';
 import { Stock } from './presentation/pages/stock/stock';
 import { Commissions } from './presentation/pages/commissions/commissions';
 
@@ -11,10 +10,6 @@ export const routes: Routes = [
     {
         path: '',
         component: Home
-    },
-    {
-        path: 'sales',
-        component: Sales
     },
     {
         path: 'commissions',
